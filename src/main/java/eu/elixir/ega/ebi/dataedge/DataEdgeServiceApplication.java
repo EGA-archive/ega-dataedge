6ef382302bdf71f99da9ba1891b5ed467d24bb3e
asenf <asenf@asenf-virtual-machine>
2018-03-16 10:31:38 +0000

    Add CORS support

--- src/main/java/eu/elixir/ega/ebi/dataedge/DataEdgeServiceApplication.java
+++ src/main/java/eu/elixir/ega/ebi/dataedge/DataEdgeServiceApplication.java
@@ -17,6 +17,8 @@ package eu.elixir.ega.ebi.dataedge;
 
 import eu.elixir.ega.ebi.dataedge.config.MyConfiguration;
 import eu.elixir.ega.ebi.dataedge.config.OAuth2ResourceConfig;
+import eu.elixir.ega.ebi.dataedge.config.SecurityConfig;
+import eu.elixir.ega.ebi.dataedge.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -30,7 +32,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@EnableCircuitBreaker
 //@EnableHystrix
 @SpringBootApplication
-@Import({MyConfiguration.class, OAuth2ResourceConfig.class})
+@Import({MyConfiguration.class, OAuth2ResourceConfig.class,
+         WebConfig.class, SecurityConfig.class})
 @EnableSwagger2
 @EnableEurekaClient
 public class DataEdgeServiceApplication {
---
+++ src/main/java/eu/elixir/ega/ebi/dataedge/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 ELIXIR EGA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.elixir.ega.ebi.dataedge.config;
+
+import com.google.common.collect.ImmutableList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ *
+ * @author asenf
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+        http.cors();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
