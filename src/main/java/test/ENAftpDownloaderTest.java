cbb1a1848867e8b546158874e22a277a5bee2817
Dilschat <dilschat@yandex.ru>
2018-07-13 11:43:57 +0300

    test added

--- src/main/java/test/ENAftpDownloaderTest.java
+++ src/main/java/test/ENAftpDownloaderTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import eu.elixir.ega.ebi.dataedge.service.ena.htsget.service.internal.ENAFtpDownloader;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -11,21 +12,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class ENAftpDownloaderTest {
     /**
      * For running this test you need to put into root folder of project ERR1777637_1.fastq.gz
-     from ftp.sra.ebi.ac.uk/vol1/fastq/ERR177/007/ERR1777637/ERR1777637_1.fastq.gz
+     from ftp.sra.ebi.ac.uk/vol1/fastq/SRR921/SRR921499/SRR921499.fastq.gz
      **/
     @Test
     public void downloadingTest1() throws IOException {
-        String pathToExpectedFile = "C:\\Users\\dilsc\\Desktop\\ega-dataedge\\ERR1777637_1.fastq.gz";
+        String pathToExpectedFile = "/Users/dilsatsalihov/Desktop/gsoc/ega-dataedge/SRR921499.fastq.gz";
         File expectedFile = new File(pathToExpectedFile);
         ENAFtpDownloader downloader = new ENAFtpDownloader();
-        InputStream receivedStream = downloader.getFastqFile("ftp.sra.ebi.ac.uk/vol1/fastq/ERR177/007/ERR1777637/ERR1777637_1.fastq.gz");
+        InputStream receivedStream = downloader.getFastqFile("ftp.sra.ebi.ac.uk/vol1/fastq/SRR921/SRR921499/SRR921499.fastq.gz");
         File downloadedFile = writeToFile(receivedStream);
-        assertTrue(fileEquals(expectedFile,downloadedFile));
+        assertTrue(FileUtils.contentEquals(expectedFile,downloadedFile));
     }
 
     public File writeToFile(InputStream inputStream){
         OutputStream outputStream = null;
-        File file = new File("C:\\Users\\dilsc\\Desktop\\ega-dataedge\\test-ERR1777637_1.fastq.gz");
+        File file = new File("/Users/dilsatsalihov/Desktop/gsoc/ega-dataedge/2.4MB_test.zip");
         try {
 
             // write the inputStream to a FileOutputStream
@@ -64,65 +65,6 @@ class ENAftpDownloaderTest {
         return file;
     }
 
-    public boolean fileEquals(File file1, File file2) throws IOException {
-        boolean areFilesIdentical = true;
-        FileInputStream fis1 = new FileInputStream(file1);
-        FileInputStream fis2 = new FileInputStream(file2);
-        int i1 = fis1.read();
-        int i2 = fis2.read();
-        while (i1 != -1) {
-            if (i1 != i2) {
-                areFilesIdentical = false;
-                break;
-            }
-            i1 = fis1.read();
-            i2 = fis2.read();
-        }
-        fis1.close();
-        fis2.close();
-        return areFilesIdentical;
-    }
 
-
-
-    public static boolean isSame( InputStream input1,
-                                  InputStream input2 ) throws IOException {
-        boolean error = false;
-        try {
-            byte[] buffer1 = new byte[1024];
-            byte[] buffer2 = new byte[1024];
-            try {
-                int numRead1 = 0;
-                int numRead2 = 0;
-                while (true) {
-                    numRead1 = input1.read(buffer1);
-                    numRead2 = input2.read(buffer2);
-                    if (numRead1 > -1) {
-                        if (numRead2 != numRead1) return false;
-                        // Otherwise same number of bytes read
-                        if (!Arrays.equals(buffer1, buffer2)) return false;
-                        // Otherwise same bytes read, so continue ...
-                    } else {
-                        // Nothing more in stream 1 ...
-                        return numRead2 < 0;
-                    }
-                }
-            } finally {
-                input1.close();
-            }
-        } catch (IOException e) {
-            error = true; // this error should be thrown, even if there is an error closing stream 2
-            throw e;
-        } catch (RuntimeException e) {
-            error = true; // this error should be thrown, even if there is an error closing stream 2
-            throw e;
-        } finally {
-            try {
-                input2.close();
-            } catch (IOException e) {
-                if (!error) throw e;
-            }
-        }
-    }
 
 }
